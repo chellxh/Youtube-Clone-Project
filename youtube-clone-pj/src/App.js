a19b1f1562fc2b609674ea317db9a49f286ac001
thandi bristol <thandibristol@thandis-air.mynetworksettings.com>
2023-06-01 11:02:37 -0400

    sigh

--- youtube-clone-pj/src/App.js
+++ youtube-clone-pj/src/App.js
@@ -1,15 +1,9 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Components/Header/Header";
-<<<<<<< HEAD
-import Home from "./Components/Home";
+import Home from "./Components/Home/Home";
 import Video from "./Components/Video/Video";
 import About from "./Components/About/About";
-=======
-import Home from "./Components/Home/Home";
-import Video from "./Components/Videos/Video";
-import About from './Components/About/About'
->>>>>>> quick-change
 
 function App() {
   return (
